ef46a75fd79e625c77d13a1e0f59235f297b28f3
Bryan Tjandra <tjandra.benedict@gmail.com>
2023-07-06 02:04:04 +0800

    add basic authentication

--- app.js
+++ app.js
@@ -3,18 +3,22 @@ const mongoose = require("mongoose");
 const app = express();
 const ejsMate = require("ejs-mate");
 const path = require("path");
-const Campground = require("./models/campground")
-const methodOverride = require("method-override")
-const catchAsync = require("./utils/catchAsync")
-const Joi = require("joi")
-const ExpressError  = require("./utils/ExpressError")
-const {campgroundSchema, reviewSchema} = require("./schemas.js")
-const Review = require("./models/review")
+const Campground = require("./models/campground");
+const methodOverride = require("method-override");
+const catchAsync = require("./utils/catchAsync");
+const Joi = require("joi");
+const ExpressError  = require("./utils/ExpressError");
+const {campgroundSchema, reviewSchema} = require("./schemas.js");
+const Review = require("./models/review");
 const session = require("express-session");
-const flash = require("connect-flash")
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
-const campgrounds = require("./routes/campgrounds")
-const reviews = require("./routes/reviews")
+const userRoutes = require("./routes/users");
+const campgroundRoutes = require("./routes/campgrounds")
+const reviewRoutes = require("./routes/reviews")
 
 mongoose.connect('mongodb://127.0.0.1:27017/camp-scout',{
     useNewUrlParser: true,
@@ -45,18 +49,26 @@ const sessionConfig = {
         maxAge: 1000*60*60*24*7
     }
 }
-app.use(session(sessionConfig))
+app.use(session(sessionConfig));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.use((req,res,next) => {
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 })
 
 
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews)
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes)
 
 app.get('/', (req,res) => {
     res.render("home")
